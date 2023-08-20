1252a16e88b9b074e09df186b4975d275d7e0679
ChocoLZS <chocoielzs@gmail.com>
2023-08-20 06:09:13 +0000

    init: Nextjs + React + tailwind

--- next.config.js
+++ next.config.js
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  webpack(config, { isServer }) {
+    // ref: https://react-svgr.com/docs/webpack/
+    config.module.rules.push({
+      test: /\.svg$/i,
+      type: 'asset/resource',
+      resourceQuery: /url/, // *.svg?url
+    });
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      resourceQuery: { not: [/url/, /raw/] }, // exclude react component if *.svg?url
+      loader: require.resolve('@svgr/webpack'),
+      options: {
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'preset-default',
+              params: {
+                overrides: {
+                  removeViewBox: false,
+                  cleanupIds: false,
+                },
+              },
+            },
+          ],
+        },
+      },
+    });
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
