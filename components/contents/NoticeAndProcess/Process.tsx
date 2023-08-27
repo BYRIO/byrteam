57184eb6770e86b22f08c27eb740afadb484dea3
siman <siman@qiulishe.com>
2023-08-27 17:05:14 +0800

    [process]流程图大小调整

--- components/contents/NoticeAndProcess/Process.tsx
+++ components/contents/NoticeAndProcess/Process.tsx
@@ -6,7 +6,9 @@ export default function Process() {
   return (
     <motion.div layout className="mx-auto my-6 max-w-[1440px]">
       <ProcessDesktop className="w-full max-sm:hidden" />
-      <ProcessMobile className="w-[95%] sm:hidden" />
+      <div className="-mt-[80px] -mb-[100px] flex justify-center">
+        <ProcessMobile className="w-[85%] sm:hidden" />
+      </div>
     </motion.div>
   );
 }
