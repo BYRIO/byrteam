e0818c89f686f167e8ab077db24c973c5725b2f5
ChocoLZS <chocoielzs@gmail.com>
2023-08-27 10:40:30 +0000

    fix: padding fix

--- components/contents/NoticeAndProcess/Process.tsx
+++ components/contents/NoticeAndProcess/Process.tsx
@@ -6,7 +6,7 @@ export default function Process() {
   return (
     <motion.div layout className="mx-auto my-6 max-w-[1440px]">
       <ProcessDesktop className="h-fit w-full max-md:hidden" />
-      <ProcessMobile className="h-fit w-full px-4 md:hidden" />
+      <ProcessMobile className="h-fit w-full max-sm:px-4 sm:px-10 md:hidden" />
     </motion.div>
   );
 }
