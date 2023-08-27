3d25da45f0aeb2d1afa1e5570bcad886a6b90c63
siman <siman@qiulishe.com>
2023-08-27 15:56:41 +0800

    [progress]增加移动端流程图

--- components/contents/NoticeAndProcess/Process.tsx
+++ components/contents/NoticeAndProcess/Process.tsx
@@ -1,10 +1,12 @@
 import ProcessDesktop from '@/assets/images/senpai-saying/process-desktop.svg';
+import ProcessMobile from '@/assets/images/senpai-saying/process-mobile.svg';
 import { motion } from 'framer-motion';
 
 export default function Process() {
   return (
     <motion.div layout className="mx-auto my-6 max-w-[1440px]">
-      <ProcessDesktop className="w-full" />
+      <ProcessDesktop className="w-full max-sm:hidden" />
+      <ProcessMobile className="w-[95%] sm:hidden" />
     </motion.div>
   );
 }
