b3726492ba4e600d96fd02222c842c030a6c5005
ChocoLZS <chocoielzs@gmail.com>
2023-09-03 02:54:16 +0000

    feat: introduce 404 & error page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function _404() {
+  return (
+    <section className="flex h-[calc(100vh-168px)] items-center justify-center bg-gradient-to-b from-slate-400 to-[#F5F5F5]">
+      <h1 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-smiley">
+        你说得对，但是托托是一款...
+      </h1>
+    </section>
+  );
+}
