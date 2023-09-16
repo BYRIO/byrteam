df55082be00a51611a2db7c61ea3ca95a24ec16a
ChocoLZS <chocoielzs@gmail.com>
2023-09-16 05:40:09 +0000

    feat: design department animation

--- components/contents/Department/NewDesignDescription.tsx
+++ components/contents/Department/NewDesignDescription.tsx
@@ -2,27 +2,130 @@ import DesignTitle from '@/assets/images/departments/design/icon-title.svg';
 import IconSaturn from '@/assets/images/departments/design/icon-saturn.svg';
 import clsx from 'clsx';
 
+import { useRef } from 'react';
+
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  useMotionValue,
+  useVelocity,
+  useAnimationFrame,
+} from 'framer-motion';
+import { wrap } from '@motionone/utils';
+
+import TextGraphic from '@/assets/images/departments/design/text-graphic.svg';
+import TextProduct from '@/assets/images/departments/design/text-product.svg';
+import TextInteraction from '@/assets/images/departments/design/text-interaction.svg';
+import TextWhatever from '@/assets/images/departments/design/text-whatever.svg';
+
+interface ParallaxProps {
+  children: string;
+  baseVelocity: number;
+}
+
+function ParallaxText({ children, baseVelocity = 100 }: ParallaxProps) {
+  const baseX = useMotionValue(0);
+  const { scrollY } = useScroll();
+  const scrollVelocity = useVelocity(scrollY);
+  const smoothVelocity = useSpring(scrollVelocity, {
+    damping: 50,
+    stiffness: 400,
+  });
+  const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
+    clamp: false,
+  });
+
+  /**
+   * This is a magic wrapping for the length of the text - you
+   * have to replace for wrapping that works for you or dynamically
+   * calculate
+   */
+  const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
+
+  const directionFactor = useRef<number>(1);
+  useAnimationFrame((t, delta) => {
+    let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
+
+    /**
+     * This is what changes the direction of the scroll once we
+     * switch scrolling directions.
+     */
+    if (velocityFactor.get() < 0) {
+      directionFactor.current = -1;
+    } else if (velocityFactor.get() > 0) {
+      directionFactor.current = 1;
+    }
+
+    moveBy += directionFactor.current * moveBy * velocityFactor.get();
+
+    baseX.set(baseX.get() + moveBy);
+  });
+
+  /**
+   * The number of times to repeat the child text should be dynamically calculated
+   * based on the size of the text and viewport. Likewise, the x motion value is
+   * currently wrapped between -20 and -45% - this 25% is derived from the fact
+   * we have four children (100% / 4). This would also want deriving from the
+   * dynamically generated number of children.
+   */
+  return (
+    <div
+      className={clsx(
+        'flex flex-nowrap overflow-hidden whitespace-nowrap tracking-[-2px]',
+        'text-6xl',
+        'sm:text-7xl',
+        'md:text-8xl',
+        'lg:text-9xl'
+      )}
+    >
+      <motion.div style={{ x }}>
+        <span>{children} </span>
+        <span>{children} </span>
+        <span>{children} </span>
+        <span>{children} </span>
+      </motion.div>
+    </div>
+  );
+}
+
 export default function NewDesignDescription() {
   return (
     <section
-      className={clsx('relative h-full', {
+      className={clsx('relative h-full overflow-hidden', {
         'text-[#0067D1]': true,
       })}
     >
       <div
         className={clsx(
           'absolute inset-0 -mr-1 flex flex-col justify-center text-right font-poppins-800 italic opacity-20',
-          'gap-5 text-6xl',
-          'sm:gap-5 sm:text-7xl',
+          'gap-12 text-6xl',
+          'sm:gap-8 sm:text-7xl',
           'md:gap-5 md:text-8xl',
           'lg:justify-between lg:text-9xl'
         )}
       >
-        <div>GRAPHIC</div>
-        <div>PRODUCT</div>
-        <div>INTERACTION</div>
-        <div>WHATEVER</div>
+        <ParallaxText baseVelocity={-5}>GRAPHIC</ParallaxText>
+        <ParallaxText baseVelocity={5}>PRODUCT</ParallaxText>
+        <ParallaxText baseVelocity={-5}>INTERACTION</ParallaxText>
+        <ParallaxText baseVelocity={5}>WHATEVER</ParallaxText>
       </div>
+      {/* <div
+        className={clsx(
+          'absolute inset-0 -mr-1 flex flex-col flex-wrap justify-center text-right opacity-20',
+          '[&>svg]:self-end',
+          'gap-12 [&>svg]:max-h-12',
+          'sm:gap-10 sm:[&>svg]:max-h-16',
+          'md:gap-12 md:[&>svg]:max-h-20',
+          'lg:justify-between lg:[&>svg]:max-h-24'
+        )}
+      >
+        <TextGraphic />
+        <TextProduct />
+        <TextInteraction />
+        <TextWhatever />
+      </div> */}
       <article
         className={clsx(
           'relative h-full',
